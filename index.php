32fb1820d3f041945853fcb079bbd98e3ff58e85
Janhavi Anap <63518181+JanhaviAnap@users.noreply.github.com>
2022-04-22 04:25:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<head>
+<title>faqs</title>
+<link href='https://fonts.googleapis.com/css?family=Cagliostro' rel='stylesheet'>
+<script src="https://kit.fontawesome.com/e452450e48.js" crossorigin="anonymous"></script>
+
+<style>
+
+.header {
+  padding-top: 0.75rem;
+    padding-right: 2rem;
+    padding-bottom: 0.75rem;
+    padding-left: 2rem;
+ 
+  background: #311c61;
+  background-size: 100%;
+  position: relative;
+  color: white;
+  font-size: 10px;
+}
+
+.bg-image {
+  /* The image used */
+  background-image: url("photographer.jpg");
+
+  /* Add the blur effect */
+  filter: blur(2px);
+  -webkit-filter: blur(2px);
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.bg-text {
+  background-color: rgb(0,0,0);
+   /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); 
+  /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  /* border: 3px solid #f1f1f1; */
+  position: absolute;
+  top: 30%;
+  left: 35%;
+  transform: translate(-30%, -50%);
+  z-index: 2;
+  width: 40%;
+  padding: 20px;
+  font-size:25px;
+  text-align: center;
+}
+
+</style>
+
+<body>
+<div class="header">
+<!-- <img src="/home/nidhik/Desktop/hanf=dymen/professional-handyman-logo-silhouettes-tools-repair-geometric-violet-background-stock-flat-design-vector-illustration-114223251.jpg" alt="logo" style="float:left;width:83px;height:83px;"> -->
+<h1 align="left">HANDYMAN</h1>
+  <!-- <h4 align="left">Career Counselling & Guidance</h4> -->
+</div>
+
+<div class="bg-image">
+  <img src="bgimg.jpg" style="width:100%;  height:700px;"  class="img">
+</div>
+
+<div class="bg-text">  
+  <h1 class="line-bottom">Service at your door step</h1>
+</div>
+
+<script type="text/javascript">
+  (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"3f400acfc62a41ad91fc23392540ecaf8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+</script>
+
+</body>
+
+<!-- <div class="footer">
+<a href="about.html">About Us</a><br>
+<p>Connect with us:
+<a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href=""><i class="fa fa-whatsapp" aria-hidden="true"></i></a></p>
+<details>
+<summary>Contact Us</summary><p><i class="fa fa-phone" aria-hidden="true"></i>1234567890<br>
+<i class="fa fa-envelope-open" aria-hidden="true"></i>dreambid@gmail.com
+</p>
+</details><br>
+<div class="col">
+<a href="testimonials.html">Feedback</a>
+<p><a href="teamm.html">Meet the team</a></p>
+<p><a href="faq.html">FAQs</a></p></div>
+<br>
+<p align="center">&copy; All Rights Resered, DreamBig 2020.</p>
+<p align="center"> Made with<i class="fa fa-heart" aria-hidden="true"></i>by Chirag Hegde, Nidhi Singh &amp; Nidhi Heniya.</p> -->
+
+</html>
